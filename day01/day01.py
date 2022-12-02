7a810f4c237b2256c32fc7994021c8697d4d1d2f
Pratik <pmaharjan@juniper.net>
2022-12-01 22:59:40 -0500

    Initial commit

---
+++ day01/day01.py
@@ -0,0 +1,24 @@
+lines = []
+part1val = 0
+with open("day01.txt") as file:
+    for line in file:
+        line = line.strip()
+        lines.append(line)
+
+elves = []
+current = 0
+for line in lines:
+    if line != '':
+        current = current + int(line)
+    else:
+        elves.append(current)
+        current = 0
+elves.append(current)
+# print(f"Unsorted Elves:\n {elves}")
+elves.sort()
+# print(f"Sorted Elves:\n {elves}")
+part1val = elves[-1]
+print(f"Part 1: {part1val}")
+
+part2val = sum(elves[-3:])
+print(f"Part 2: {part2val}")
