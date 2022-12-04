46489b5464a6f68263141d7264d68dd3b165a133
Pratik <pmaharjan@juniper.net>
2022-12-04 18:41:39 -0500

    complete day 3

--- day03/day03.py
+++ day03/day03.py
@@ -7,12 +7,20 @@ def readFile():
     return lines
 
 
-def get_common(first_part, second_part):
-    in_both = ''
-    for item in first_part:
-        if item in second_part and item not in in_both:
-            in_both += item
-    return in_both
+def get_common(first_part, second_part, third_part=None):
+    if third_part is None:
+        in_both = ''
+        for item in first_part:
+            if item in second_part and item not in in_both:
+                in_both += item
+        return in_both
+    else:
+        in_three = ''
+        for item in first_part:
+            if item in second_part and item not in in_three:
+                if item in third_part and item not in in_three:
+                    in_three += item
+        return in_three
 
 
 def get_priority(common):
@@ -22,6 +30,14 @@ def get_priority(common):
     else:
         priority = ascii_value - 38
     return priority
+
+
+def get_groups(sacks):
+    groups = []
+    for i in range(0, len(sacks), 3):
+        group = [sacks[i], sacks[i + 1], sacks[i + 2]]
+        groups.append(group)
+    return groups
 
 
 def partOne():
@@ -35,14 +51,20 @@ def partOne():
 
 
 def partTwo():
-    pass
+    rucksacks = readFile()
+    elf_groups = get_groups(rucksacks)
+    priority_sum = 0
+    for group in elf_groups:
+        common = get_common(group[0], group[1], group[2])
+        priority_sum += get_priority(common)
+    return priority_sum
 
 
 def main():
     part1val = partOne()
     print(f"Part 1: {part1val}")
-    # part2val = partTwo()
-    # print(f"Part 1: {part2val}")
+    part2val = partTwo()
+    print(f"Part 2: {part2val}")
 
 
 if __name__ == "__main__":
