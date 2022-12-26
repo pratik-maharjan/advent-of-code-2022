87c4e5e1f7bb2a801e5fee043ad9a1a555944f22
Pratik <pmaharjan@juniper.net>
2022-12-26 13:27:55 -0500

    complete day 5 part 2

--- day05/day05.py
+++ day05/day05.py
@@ -74,16 +74,32 @@ def strip_instructions(input):
     return result
 
 
-def crane(num, start, end, arr):
+def crane_9000(num, start, end, arr):
     for i in range(0, num):
         item = arr[start-1].pop()
         arr[end-1].append(item)
     return arr
 
 
-def play(command, state):
-    for c in command:
-        crane(int(c[0]), int(c[1]), int(c[2]), state)
+def crane_9001(num, start, end, arr):
+    buffer = []
+    for i in range(0, num):
+        item = arr[start-1].pop()
+        buffer.append(item)
+    buffer.reverse()
+    arr[end-1] = arr[end-1] + buffer
+    return arr
+
+
+def play(command, state, part):
+    if part == 1:
+        for c in command:
+            crane_9000(int(c[0]), int(c[1]), int(c[2]), state)
+    elif part == 2:
+        for c in command:
+            crane_9001(int(c[0]), int(c[1]), int(c[2]), state)
+    else:
+        return None
     return state
 
 
@@ -106,26 +122,35 @@ def partOne():
     instructions = get_instructions(strip_input)
     arr_instructions = strip_instructions(instructions)
 
-    final_state = play(arr_instructions, start_state)
+    final_state = play(arr_instructions, start_state, 1)
     answer = get_result(final_state)
-    # print(layout)
-    # print(start_state)
-    # print(total_stacks)
-    # print(instructions)
-    # print(arr_instructions)
     print("Final: {}".format(final_state))
     return answer
 
 
 def partTwo():
-    pass
+    strip_input = readFile(True)
+    raw_input = readFile(False)
+    layout = get_layout(raw_input)
+    last_row = get_last_row(layout)
+    total_stacks = get_total_stacks(last_row)
+    start_state = get_stack_layout(layout[:-1], total_stacks, last_row)
+    print("Start: {}".format(start_state))
+    instructions = get_instructions(strip_input)
+    arr_instructions = strip_instructions(instructions)
+
+    final_state = play(arr_instructions, start_state, 2)
+    answer = get_result(final_state)
+    print("Final: {}".format(final_state))
+    return answer
 
 
 def main():
     part1val = partOne()
     print(f"Part 1: {part1val}")
-    # part2val = partTwo()
-    # print(f"Part 2: {part2val}")
+    print()
+    part2val = partTwo()
+    print(f"Part 2: {part2val}")
 
 
 if __name__ == "__main__":
